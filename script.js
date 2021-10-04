6c7ed76a52d6f4e01aab842f0d48ac1b47924895
=Salman nazeer <salmanselu2002@gmail.com>
2021-10-04 13:03:46 +0530

    Got it to work

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 
-const scripts = ["Banana", "Orange", "Apple", "Mango"];
 
-
+let scripts = [
+    {text:"hello there",time:2000},
+    {text:"hello ",time:2000},
+    {text:"hi",time:2000},
+];
+timeout = 0;
 Text = document.getElementById("demo");
-scripts.forEach((element) => {Text.innerHTML = element;})
+
+scripts.forEach(
+    (script) => {
+        console.log(script.text);
+        timeout = timeout + script.time;
+        setTimeout(() => {
+            Text.innerHTML = script.text;
+        }, timeout);
+
+    }
+);
 
 
 
